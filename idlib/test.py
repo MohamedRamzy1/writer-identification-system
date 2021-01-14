f0740e04ced516b5a2f5f5e766d32e813a783724
DarkGeekMS <mohamedshawky911@gmail.com>
2021-01-14 05:56:01 +0200

    update README and train pipeline

--- idlib/test.py
+++ idlib/test.py
@@ -1,7 +1,7 @@
 from idlib.dataset.test_loader import TestLoader
 from idlib.classifier import models
 from idlib.feature_extractor.lpq_features import LPQFeatureExtractor
-from idlib.preprocessor.form_preparation import FormPreparator
+from idlib.preprocessor.form_preparator import FormPreparator
 
 def test(data_dir='data/test_samples/'):
     # TODO : implement final test driver
--- idlib/train.py
+++ idlib/train.py
@@ -5,7 +5,7 @@ from idlib.feature_extractor.lbp_features import LBPFeatureExtractor
 from idlib.feature_extractor.lpq_features import LPQFeatureExtractor
 from idlib.feature_extractor.glcm_features import GLCMFeaturesExtractor
 from idlib.feature_extractor.pca import PCA
-from idlib.preprocessor.form_preparation import FormPreparator
+from idlib.preprocessor.form_preparator import FormPreparator
 
 import torch
 from sklearn.svm import SVC
@@ -25,6 +25,8 @@ def complete_train(data_dir='data/'):
     dataloader.build_dataset()
     # initialize LBP feature extractor
     lbp_extractor = LBPFeatureExtractor()
+    # initialize form preparator
+    form_processor = FormPreparator(denoise=True)
     # get complete train/validation dataset
     xtrain, xvalid, ytrain, yvalid = dataloader.get_complete_data(forms_per_writer=5, test_split=0.2)
     # read and prepare features for train images
@@ -32,7 +34,7 @@ def complete_train(data_dir='data/'):
     ytrain_labels = list()
     for img_name, label in zip(xtrain, ytrain):
         img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-        lines, bin_lines = FormPreparator.prepare_form(img)
+        lines, bin_lines = form_processor.prepare_form(img)
         features = lbp_extractor.fit(lines, bin_lines)
         xtrain_features.extend(features)
         ytrain_labels.extend([label]*len(features))
@@ -41,7 +43,7 @@ def complete_train(data_dir='data/'):
     yvalid_labels = list()
     for img_name, label in zip(xvalid, yvalid):
         img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-        lines, bin_lines = FormPreparator.prepare_form(img)
+        lines, bin_lines = form_processor.prepare_form(img)
         features = lbp_extractor.fit(lines, bin_lines)
         xvalid_features.append(features)
         yvalid_labels.append(label)
@@ -62,39 +64,4 @@ def complete_train(data_dir='data/'):
     print (f'svm accuracy: {accuracy_score(yvalid_labels, predictions)*100}%')
 
 def sampled_train(data_dir='data/'):
-    # initialize and build dataloader
-    dataloader = TrainLoader(data_dir=data_dir)
-    dataloader.build_dataset()
-    # initialize LBP feature extractor
-    lbp_extractor = LBPFeatureExtractor()
-    for case in range(100):
-        # get complete train/validation dataset
-        xtrain, xvalid, ytrain, yvalid = dataloader.get_data_samples(num_writers=3, num_train_samples=2)
-        # read and prepare features for train images
-        xtrain_features = list()
-        ytrain_labels = list()
-        for img_name, label in zip(xtrain, ytrain):
-            img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-            lines, bin_lines = FormPreparator.prepare_form(img)
-            features = lbp_extractor.fit(lines, bin_lines)
-            xtrain_features.extend(features)
-            ytrain_labels.extend([label]*len(features))
-        # read and prepare features for validation images
-        xvalid_features = list()
-        yvalid_labels = list()
-        for img_name, label in zip(xvalid, yvalid):
-            img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-            lines, bin_lines = FormPreparator.prepare_form(img)
-            features = lbp_extractor.fit(lines, bin_lines)
-            xvalid_features.append(features)
-            yvalid_labels.append(label)
-        # perform classical model grid search
-        clf = SVC(C=5.0, gamma='auto', probability=True, verbose=True)
-        clf.fit(xtrain_features, ytrain_labels)
-        # get classical model predictions
-        predictions = list()
-        for form in xvalid_features:
-            lines_prob = clf.predict_proba(form)
-            lines_prob = np.sum(lines_prob, axis=0)
-            predictions.append(clf.classes_[np.argmax(lines_prob)])
-    print (f'svm accuracy: {accuracy_score(yvalid_labels, predictions)*100}%')
+    pass
